29b7b36bd1359f9e80aab3037e82e556b492b0d1
장현수 <wkdalsrud113@naver.com>
2024-10-29 12:32:10 +0900

    test:test

--- frontApp/App.tsx
+++ frontApp/App.tsx
@@ -87,7 +87,7 @@ function App(): React.JSX.Element {
           </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next: a<div>aaa</div>
-            "v" "a" "a"
+            "v" "a" "a"asf "d"
           </Section>
           <LearnMoreLinks />
         </View>
