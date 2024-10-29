260f7122886f62c684ce830bc7320be5ee757d8b
장현수 <wkdalsrud113@naver.com>
2024-10-29 17:08:42 +0900

    feature: router

---
+++ FrontendApp/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Login(): React.JSX.Element {
+  return (
+    <View>
+      <Text>로그인 페이지</Text>
+    </View>
+  );
+}
+
+export default Login;
