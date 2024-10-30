01f51fd1fd4d18f978f3e6770255e34f4d173fd5
장현수 <wkdalsrud113@naver.com>
2024-10-30 10:13:08 +0900

    fix: headerShown

--- FrontendApp/src/pages/Login.tsx
+++ FrontendApp/src/pages/Login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-function Login(): React.JSX.Element {
+const Login = (): React.JSX.Element => {
   return (
     <View>
       <Text>로그인 페이지</Text>
     </View>
   );
-}
+};
 
 export default Login;
