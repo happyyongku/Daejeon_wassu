4a62d123d329fb43742b274a11b5f6ac04b62b34
kimhaesu <gotnsla12@naver.com>
2024-11-06 13:24:07 +0900

    feat: 회원가입 기능구현

--- frontend/project105/src/app/signup/page.tsx
+++ frontend/project105/src/app/signup/page.tsx
@@ -5,6 +5,12 @@ export default function Page() {
   return (
     <div className={style.container}>
       {/* <div>회원가입 페이지</div> */}
+      <div className={style.logo_container}>
+        <img className={style.logotext} src="/images/logotext.png" alt="" />
+        <p className={style.logo_description}>
+          대전의 숨겨진 매력을 찾아 떠나는 여행
+        </p>
+      </div>
       <SignupForm />
     </div>
   );
