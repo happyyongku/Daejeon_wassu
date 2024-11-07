1c5a35add8a0c9646d3619650e8f1a78fc681d6d
장현수 <wkdalsrud113@naver.com>
2024-11-07 16:01:33 +0900

    feat: map

--- FrontendApp/src/pages/Map.tsx
+++ FrontendApp/src/pages/Map.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+
+const coordinate = {
+  latitude: 36.367771,
+  longitude: 127.3886019,
+};
 
 const Map: React.FC = () => {
   return (
-    <View style={{flex: 1}}>
-      <Text>졸려</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: coordinate.latitude,
+          longitude: coordinate.longitude,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.0001,
+        }}>
+        <Marker
+          coordinate={{
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
