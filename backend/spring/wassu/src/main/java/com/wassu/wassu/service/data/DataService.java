28e868564fad296fdd7d3e06bc86cfa277827252
happyyongku <yonggu1951@gmail.com>
2024-11-10 22:31:19 +0900

    temp

---
+++ backend/spring/wassu/src/main/java/com/wassu/wassu/service/data/DataService.java
@@ -0,0 +1,12 @@
+package com.wassu.wassu.service.data;
+
+import java.io.IOException;
+import java.util.List;
+import com.opencsv
+
+public class DataService {
+
+    public <T> List<T> parseCsvToBean(String filePath, Class<T> type) throws IOException {
+        try (CSVReader)
+    }
+}
