179a92a9ec8d78ae2c0703818b6eb3f3b124435b
kimhaesu <gotnsla12@naver.com>
2024-11-14 10:25:34 +0900

    feat: 로그인모달디자인

--- frontend/project105/src/components/main/tologinmodal.tsx
+++ frontend/project105/src/components/main/tologinmodal.tsx
@@ -9,8 +9,17 @@ const LoginModal: React.FC<LoginModalProps> = ({ onLoginClick }) => {
   return (
     <div className={style.modalOverlay}>
       <div className={style.modalContent}>
-        <p>로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?</p>
-        <button onClick={onLoginClick}>로그인</button>
+        <div className={style.imgbox}>
+          <img className={style.img1} src="images/logo.png" alt="" />
+          <img className={style.img2} src="/images/logotext.png" alt="" />
+        </div>
+        <h1 className={style.h1}>로그인을 안하셨네요 !</h1>
+        <p className={style.content}>
+          로그인인 하고, 다양한 기능을 사용해보세요
+        </p>
+        <button className={style.button} onClick={onLoginClick}>
+          로그인
+        </button>
       </div>
     </div>
   );
